5ddcaeeb1aced9fdaaca6c681194b0e2f917fae9
Randall Sturgis <randallcsturgis@gmail.com>
2016-04-03 17:40:01 -0700

    Init.

--- main.go
+++ main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"strings"
+	"fmt"
 )
 
 func main() {
-	input, err := ioutil.ReadFile("icws94.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	lines := strings.Split(string(input), "\n")
-
-	for i, line := range lines {
-		if strings.Contains(line, "]") {
-			lines[i] = "LOL"
-		}
-	}
-	output := strings.Join(lines, "\n")
-	err = ioutil.WriteFile("myfile", []byte(output), 0644)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	fmt.Println("This is gMARS; a go implementation of the CoreWars game intended to be hosted as a web service.")
 }
