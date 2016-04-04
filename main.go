aa662181cf31ca2dbc585f15e6b45bbf71416ad4
Randall Sturgis <randall@sturgis.io>
2016-04-04 04:40:16 +0000

    test print function

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/randall2602/gmars/core"
+	"github.com/randall2602/gmars/display"
 )
 
 func main() {
 
 	config := core.ConfigureKOTH()
+	config.CoreSize = 80 // for easier debugging
 	MyCore := core.Initialize(config)
-	fmt.Println(MyCore)
+
+	// print core n addresses at a time
+	display.Print(&MyCore, 8)
 }
