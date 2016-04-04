aec698bf455df6ef2f9d9689ad932b68f328d04a
Randall Sturgis <randall@sturgis.io>
2016-04-04 05:34:46 +0000

    read warriors from file, compile, load

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"github.com/randall2602/gmars/core"
 	"github.com/randall2602/gmars/display"
+	"github.com/randall2602/gmars/loader"
 )
 
 func main() {
@@ -12,5 +15,13 @@ func main() {
 	MyCore := core.Initialize(config)
 
 	// print core n addresses at a time
+	display.Print(&MyCore, 8)
+
+	imp := loader.Compile(ioutil.ReadFile("imp.redcode"))
+	dwarf := loader.Compile(ioutil.ReadFile("dwarf.redcode"))
+	warriors := []loader.Warrior{imp, dwarf}
+
+	loader.Load(warriors, &MyCore, config)
+
 	display.Print(&MyCore, 8)
 }
