2332ccb67dbda7a56ff465bebdb87f1e389b6f3d
Anonymous <anonymous@noemail.com>
2016-04-19 03:57:13 +0000

    test New() scanner

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 //  Package main is the controller of the gMARS system. It uses the Core, Compiler, and 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Warrior struct {}
+
+func (w *Warrior) unload() (byte, error) {
+  var c byte
+  var err error
+  return c, err
+}
 
 func main() {
   fmt.Println("Hello world")
+  w := Warrior{}
+  myScanner := New("my scanner", w)
 }
