dae46b841e7d467af01f612627d864c53fdf2719
Randall Sturgis <randall@sturgis.io>
2016-07-30 04:37:08 +0000

    allow gmars.go to lex multiple files

--- gmars.go
+++ gmars.go
@@ -5,30 +5,45 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-
+	//"github.com/randall2602/gmars/core"
+	//"github.com/randall2602/gmars/exec"
 	"github.com/randall2602/gmars/lex"
+	//"github.com/randall2602/gmars/load"
+	//"github.com/randall2602/gmars/parse"
 )
 
-func main() {
-	name := "test3.red"
-	file, err := os.Open(name)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "gmars: %s\n", err)
+func check(e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "gmars: %s\n", e)
 		os.Exit(1)
 	}
-	scanner := lex.New(name, bufio.NewReader(file))
+}
 
-	for tok := scanner.Next(); tok.Type != lex.EOF; tok = scanner.Next() {
-		switch tok.Type {
-		case lex.NEWLINE:
-			fmt.Print(tok, " ")
-			fmt.Print("\n")
-		case lex.LABEL, lex.INT:
-			fmt.Print(tok.Text, " ")
-		default:
-			fmt.Print(tok, " ")
+func main() {
+	names := [...]string{"./red/imp.red", "./red/dwarf.red"}
+	files := [len(names)]*os.File{}
+	for i, v := range names {
+		var err error
+		files[i], err = os.Open(v)
+		check(err)
+	}
+
+	// double check that files loaded correctly
+	for i, _ := range files {
+		buf := make([]byte, 4096)
+		n, err := files[i].Read(buf)
+		check(err)
+		fmt.Printf("%d bytes: \n%s\n\n", n, string(buf))
+		files[i].Seek(0, 0)
+	}
+	fmt.Println()
+
+	lexed_files := [len(names)][]lex.Token{}
+	for i, _ := range files {
+		scanner := lex.New(names[i], bufio.NewReader(files[i]))
+		for tok := scanner.Next(); tok.Type != lex.EOF; tok = scanner.Next() {
+			lexed_files[i] = append(lexed_files[i], tok)
 		}
 	}
-	fmt.Print(lex.Token{lex.EOF,0,""})
-	fmt.Print("\n")
+
 }
