0fdebd4b6c6fe6063283d1feb7ecf04d8459af83
Randall Sturgis <randall@sturgis.io>
2016-07-30 04:45:08 +0000

    add doc.go for godoc.org

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+
+gMARS is a MEMORY ARRAY REDCODE SIMULATOR. It is the most complex and most important part of the Redcode toolchain.
+
+gMARS implements ICWS'86, ICWS'88, ICWS'94, and ICWS'17.
+
+*/
+package main
